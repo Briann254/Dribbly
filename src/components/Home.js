be532d36c7e44ec9c3fb7b8686166cbbda88b69a
git_bnderitu <bnnderitu01@email.com>
2023-01-23 13:56:03 +0300

    editted components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+
 
 function Home() {
   return (
     <div>
-     <h2>HOME</h2> 
-     </div>
+      <h1>Welcome to Dribbly</h1>
+      <p>Hello,</p>
+    </div>
   )
 }
+
 
 export default Home
