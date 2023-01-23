cc7b9272a3201bee963917bc1b7f79b85f2cd344
git_bnderitu <bnnderitu01@email.com>
2023-01-23 15:40:45 +0300

    updated required components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
+    // <Router>
+    //   <Switch>
+    //     <Route exact path='/' component={Home} />
+    //   </Switch>
+    // </Router>
     <Home />
   );
 }
+
 export default App;
