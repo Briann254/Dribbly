9dde7e7b06b9dfe01d75866497521fcf505df8f0
ismahaneyy <liciousmayrah@gmail.com>
2023-01-24 14:45:35 +0300

    imported NavigationContext and useContext to App.js to display Homepage and YourProjects page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import { NavigationContext } from './data/NavigationContext';
+import {useContext} from 'react';
+
+function App() {
+const [currentPage,setCurrentPage] = useContext(NavigationContext)
+return (
+<>
+{currentPage}
+</>
+);
+}
+
+export default App;
