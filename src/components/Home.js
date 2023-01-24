52d9d957ee3b9de4059f14b67a66cae785c9ee9a
ismahaneyy <liciousmayrah@gmail.com>
2023-01-24 14:33:29 +0300

    added NavigationContext.js component, YourProjectsContext.js component, YourProjects.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,3 @@
-import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
 
 
-function Home() {
-    const [username, setUsername] = useState('');
-    return (
-      <div>
-        <h1>Welcome to Dribbly</h1>
-        <p>Hello, {username}</p>
-        <form>
-          <label>
-            Name:
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
 
-export default Home
