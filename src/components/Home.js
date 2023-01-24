e6bb9464ab55e95397200236faabb85c7f136e40
git_bnderitu <bnnderitu01@email.com>
2023-01-24 12:52:47 +0300

    updated src

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import { Link } from 'react-router-dom';
 
 
 function Home() {
-    const [username, setUsername] = useState('');
     return (
-      <div>
-        <h1>Welcome to Dribbly</h1>
-        <p>Hello, {username}</p>
-        <form>
-          <label>
-            Name:
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+      <div className='relative h-[480px]'>
+        <div className='absolute z-20 flex h-full flex-col items-start justify-center space-y-4 ml-12 tex-white font-bold w-[800px]'>
+          <h2 className='text-6xl'>Welcome to Your world on Dribbly</h2>
+          <p>Woo hoo Best PLace to be</p>
+          <button className='bg-pink-500 px-[12px] py-[9px] text-[15px] font-bold text-white rounded-md hover:bg-pink-400'>
+            Sign up
+          </button>
+        </div>
+        <div className='bg-black absolute opacity-70 w-full h-full'>
+          <img className='w-full h-full object-cover' src="" alt="" />
+        </div>
       </div>
+
     )
   }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {SearchIcon} from "@heroicons/react/outline";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    
+    <nav className='flex justify-between p-6 h-20'>
+      <div className='flex items-center w-[740px]'>
+      <h2 className='font-bold text-xl'> Dribbly </h2>
+      <ul className='flex flex-1 justify-between ml-8 text-[14px] text-gray-600 font-bold'>
+        <li> Home </li>
+        <li> Inspiration </li>
+        <li> About Me </li>
+        <li> Extras </li>
+      </ul>
+      </div>
+      <div className='flex items-center justify-evenly w-[28%] text-gray-600'>
+        <SearchIcon className="h-4" />
+        <h3>Sign in  /  Sign up </h3>
+        <button className='bg-pink-500 px-[12px] py-[9px] text-[15px] font-bold text-white rounded-md hover:bg-pink-400'>Start a Project </button>
+      </div>
+    </nav>
   )
 }
 
