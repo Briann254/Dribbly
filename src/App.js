461812887732b356c6d2f88f0ea45fd72a1fb642
ismahaneyy <liciousmayrah@gmail.com>
2023-01-25 09:01:44 +0300

    Revert "updated required components"
    This reverts commit cc7b9272a3201bee963917bc1b7f79b85f2cd344.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route exact path='/' component={Home} />
-    //   </Switch>
-    // </Router>
     <Home />
   );
 }
-
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
 
 
 function Home() {
-    const [username, setUsername] = useState('');
-    return (
-      <div>
-        <h1>Welcome to Dribbly</h1>
-        <p>Hello, {username}</p>
-        <form>
-          <label>
-            Name:
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Welcome to Dribbly</h1>
+      <p>Hello,</p>
+    </div>
+  )
+}
+
 
 export default Home
